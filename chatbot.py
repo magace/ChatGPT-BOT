0368785dfb55435e9cf0da75dff27c5eb86db97a
magace <magace420@gmail.com>
2023-07-31 08:06:36 -0500

    Added tenacity
    Sometimes the API would fail causing a crash.

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ from rich import print
 from unidecode import unidecode
 import requests
 import json
-
+import tenacity
 
 def load_config():
     with open("config.json") as json_file:
@@ -19,7 +19,8 @@ def send_to_discord(webhook_url, content):
         raise Exception(
             f"POST to discord returned {response.status_code}, the response is:\n{response.text}"
         )
-
+    
+@tenacity.retry(wait=tenacity.wait_fixed(2), stop=tenacity.stop_after_attempt(3))
 def ask_chat_gpt(conversation, model="gpt-3.5-turbo", max_tokens=100):
     response = openai.ChatCompletion.create(
         model=model, messages=conversation, max_tokens=max_tokens
@@ -33,7 +34,7 @@ def main():
     openai.api_key = api_key
     discord_hook = config["DISCORD_HOOK"]
     pre_message = config["PRE_MESSAGE"]
-    token_price = config["TOKEN_COST"]
+    token_price = float(config["TOKEN_COST"])
     while True:
         for name, directory in directories.items():
             question_file_path = os.path.join(directory, "question.txt")
@@ -41,19 +42,21 @@ def main():
             if os.path.exists(question_file_path):
                 with open(question_file_path, "r") as question_file:
                     user_input = question_file.readline().strip()
-                    user_input = pre_message + user_input
-                    print(f"[cyan]Question received from {name}: {user_input}[/cyan]")
+                    username, user_question = user_input.split(' ', 1)
+                    user_input = pre_message + user_question
+                    print(f"[cyan]Question received from {name} ({username}): {user_input}[/cyan]")
                     send_to_discord(
-                        discord_hook, f"Question received from {name}: {user_input}"
+                        discord_hook, f"Question received from {name} ({username}): {user_input}"
                     )
                 conversation = [{"role": "user", "content": user_input}]
                 response = ask_chat_gpt(conversation)
+
                 print(
                     f"[green]Answer from ChatGPT: {response['choices'][0]['message']['content']}[/green]"
                 )
                 send_to_discord(
                     discord_hook,
-                    f"[green]Answer from ChatGPT: {response['choices'][0]['message']['content']}[/green]",
+                    f"Answer from ChatGPT: {response['choices'][0]['message']['content']}",
                 )
                 response_obj = ask_chat_gpt(conversation)
                 response = response_obj["choices"][0]["message"]["content"].strip()
